b920f7a8071a4f65ebc5deecdecf3c783ddbaa99
Krynski <cybernetikweb@gmail.com>
2020-10-28 01:58:20 +0100

    :sparkles: feat (JavaScript/node) : parsing-data

---
+++ JavaScript/nodejs/parsing-data.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+    let body = [];
+    request.on('data', chunk => body.push(chunk));
+    request.on('end', () => {
+        body = Buffer.concat(body).toString();
+        let username = 'Unknow user'
+        if (body) {
+            username = body.split('=')[1];
+        }
+    response.setHeader('Content-type', "text/html")
+    response.write(`<h1>Hi ${username}</h1><form method="post" action="/"><input type="text" name="username"><button type="submit">Send</button></form>`);
+    response.end();
+    })
+});
+
+server.listen(3000)
