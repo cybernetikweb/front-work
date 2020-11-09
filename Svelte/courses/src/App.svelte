75970d60e3085c4a76c842beea12e629ff83d018
Krynski <cybernetikweb@gmail.com>
2020-11-09 16:01:24 +0100

    want change branch

--- Svelte/courses/src/App.svelte
+++ Svelte/courses/src/App.svelte
@@ -1,31 +1,25 @@
 <script>
-	 let name = '';
+	 export let name = '';
 	 let age = 30;
 
+	 $: uppercaseName = name.toUpperCase()
+
 	 const incrementAge = () => age += 1;
+	 const changeName = () => name = 'Rafael'
 </script>
 
 <main>
-	<h1>Hello {name}! My age is {age}</h1>
+	<h1>Hello {uppercaseName}! My age is {age}</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 	<!--Un pointeur vers la fonction qui s'éxecutera lors du click -->
 	<button on:click={incrementAge}>Change age</button>
+	<button on:click={changeName}>Change name</button>
+	<label>
+		<input type="text">
+	</label>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
 
 	@media (min-width: 640px) {
 		main {
